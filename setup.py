7b8bef5c5a17b840c654c08bc4052518bfca7e14
Mireya Montano <mmontano@carine-workstation.rag-dyn.metocean.co.nz>
2023-10-04 12:06:50 +1300

    testing no cartopy

--- setup.py
+++ setup.py
@@ -10,72 +10,73 @@ from numpy.distutils.misc_util import Configuration
 
 # Only way to get --noopt defaulted into the build environment
 # since f2py_options is not working below
-sys.argv[:] = sys.argv[:1] + ['config_fc', '--noopt'] + sys.argv[1:]
+sys.argv[:] = sys.argv[:1] + ["config_fc", "--noopt"] + sys.argv[1:]
 
 rootpath = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(*parts):
-    return open(os.path.join(rootpath, *parts), 'r').read()
+    return open(os.path.join(rootpath, *parts), "r").read()
 
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 # Dependencies.
-with open('requirements.txt') as f:
+with open("requirements_38.txt") as f:
     tests_require = f.readlines()
 install_requires = [t.strip() for t in tests_require]
 
 package_data = {
-    '': ['constituents.npz',
-         'hawaii_coast/*',
-         'roms/cdl/*.cdl',
-         'roms/cobalt/*.cdl']
+    "": ["constituents.npz", "hawaii_coast/*", "roms/cdl/*.cdl", "roms/cobalt/*.cdl"]
 }
 
-config = Configuration('')
-if platform.system() == 'Windows':
+config = Configuration("")
+if platform.system() == "Windows":
     flags = []
     libs = []
-elif platform.system() == 'Darwin':
-    flags = ['-fPIC']
-    libs = ['/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib']
+elif platform.system() == "Darwin":
+    flags = ["-fPIC"]
+    libs = ["/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"]
 else:
-    flags = ['-fPIC']
+    flags = ["-fPIC"]
     libs = []
 
-config.add_extension('oalib', sources='src/oalib.f',
-                     # f2py_options=["noopt"],
-                     extra_f77_compile_args=flags,
-                     library_dirs=libs,
-                     define_macros=[('NPY_NO_DEPRECATED_API',
-                                     'NPY_1_7_API_VERSION')])
-config.add_extension('hindices', sources='src/hindices.f',
-                     # f2py_options=["noopt"],
-                     extra_f77_compile_args=flags,
-                     library_dirs=libs,
-                     define_macros=[('NPY_NO_DEPRECATED_API',
-                                     'NPY_1_7_API_VERSION')])
+config.add_extension(
+    "oalib",
+    sources="src/oalib.f",
+    # f2py_options=["noopt"],
+    extra_f77_compile_args=flags,
+    library_dirs=libs,
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+)
+config.add_extension(
+    "hindices",
+    sources="src/hindices.f",
+    # f2py_options=["noopt"],
+    extra_f77_compile_args=flags,
+    library_dirs=libs,
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+)
 
 config = dict(
-    name=os.getenv('PACKAGE_NAME', 'seapy'),
-    version='0.8.2',
-    license='MIT',
-    description='State Estimation and Analysis in PYthon',
+    name=os.getenv("PACKAGE_NAME", "seapy"),
+    version="0.8.2",
+    license="MIT",
+    description="State Estimation and Analysis in PYthon",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Brian Powell',
-    author_email='powellb@hawaii.edu',
-    url='https://github.com/powellb/seapy',
+    long_description_content_type="text/markdown",
+    author="Brian Powell",
+    author_email="powellb@hawaii.edu",
+    url="https://github.com/powellb/seapy",
     classifiers=[
-        'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(),
     package_data=package_data,
-    ext_package='seapy.external',
-    scripts=['bin/convert_clim.py', 'bin/convert_frc.py'],
+    ext_package="seapy.external",
+    scripts=["bin/convert_clim.py", "bin/convert_frc.py"],
     install_requires=install_requires,
     zip_safe=False,
     **config.todict()
